175f8723a2ae7056155ffd4b1a62880441f651b7
wadie <wadie.farhat@simplabs.com>
2018-11-19 15:40:16 +0100

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,21 @@ var gulp = require('gulp'),
     gp_concat = require('gulp-concat'),
     gp_rename = require('gulp-rename'),
     gp_uglify = require('gulp-uglify');
+    watch = require('gulp-watch');
 
-gulp.task('jss', function(){
+gulp.task('js', function(){
     return gulp.src('./js/*.js')
         .pipe(gp_concat('main.js'))
         .pipe(gulp.dest('./js/'))
         .pipe(gp_rename('main.js'))
         .pipe(gp_uglify())
         .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('watch:js', function () {
+    gulp.watch('./js/*.js', ['js']);
+});
+
+gulp.task('watch:css', function () {
+    gulp.watch('./stylesheets/css/*.css', ['css']);
 });
