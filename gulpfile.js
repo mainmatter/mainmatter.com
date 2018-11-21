79c8a76a734af1374d00d6d732b6ffbd12e6f3f3
wadie <wadie.farhat@simplabs.com>
2018-11-21 16:34:42 +0100

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,112 @@
-var gulp = require('gulp'),
-    gp_concat = require('gulp-concat'),
-    gp_rename = require('gulp-rename'),
-    watch = require('gulp-watch');
-    terser = require('gulp-terser');
+"use strict";
 
-gulp.task('js', function(){
-    return gulp.src('./js/*.js')
-        .pipe(gp_concat('main.js'))
-        .pipe(gulp.dest('./js/'))
-        .pipe(gp_rename('main.js'))
-        .pipe(terser())
-        .pipe(gulp.dest('./js/'));
-});
+// Load plugins
+const autoprefixer = require("autoprefixer");
+const browsersync = require("browser-sync").create();
+const cp = require("child_process");
+const cssnano = require("cssnano");
+const del = require("del");
+const eslint = require("gulp-eslint");
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const uglify = require("gulp-uglify");
+const webpack = require("webpack");
+const webpackconfig = require("./webpack.config.js");
+const webpackstream = require("webpack-stream");
 
-gulp.task('watch:js', function () {
-    gulp.watch('./js/*.js', ['js']);
-});
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./_site/"
+    },
+    port: 3000
+  });
+  done();
+}
 
-gulp.task('watch:css', function () {
-    gulp.watch('./stylesheets/css/*.css', ['css']);
-});
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
 
-// Default task
-gulp.task('default', gulp.parallel('js', 'watch:js', 'watch:css'));
+// Clean assets
+function clean() {
+  return del(["./_site/js/", "./_site/stylesheets/"]);
+}
+
+// CSS task
+function css() {
+  return gulp
+    .src("./stylesheets/scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(gulp.dest("./_site/stylesheets/css/"))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest("./_site/stylesheets/css/"))
+    .pipe(browsersync.stream());
+}
+
+// Lint scripts
+function scriptsLint() {
+  return gulp
+    .src(["./js/**/*", "./gulpfile.js"])
+    .pipe(plumber())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+}
+
+// Transpile, concatenate and minify scripts
+function scripts() {
+  return (
+    gulp
+      .src(["./js/**/*"])
+      .pipe(plumber())
+      .pipe(webpackstream(webpackconfig), webpack)
+      .pipe(uglify())
+      // folder only, filename is specified in webpack config
+      .pipe(gulp.dest("./_site/js/"))
+      .pipe(browsersync.stream())
+  );
+}
+
+// Jekyll
+function jekyll() {
+  return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit" });
+}
+
+// Watch files
+function watchFiles() {
+  gulp.watch("./stylesheets/scss/**/*", css);
+  gulp.watch("./js/**/*", gulp.series(scriptsLint, scripts));
+  gulp.watch(
+    [
+      "./_includes/**/*",
+      "./_layouts/**/*"
+    ],
+    gulp.series(jekyll, browserSyncReload)
+  );
+}
+
+// Tasks
+gulp.task("css", css);
+gulp.task("js", gulp.series(scriptsLint, scripts));
+gulp.task("jekyll", jekyll);
+gulp.task("clean", clean);
+
+// build
+gulp.task(
+  "build",
+  gulp.series(clean, gulp.parallel(css, jekyll, "js"))
+);
+
+// watch
+gulp.task("watch", gulp.parallel(watchFiles, browserSync));
