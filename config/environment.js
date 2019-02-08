6ce796b419b4e8f8881e069048f6226380a0f6f6
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-02-08 17:57:33 +0100

    add services to module config

--- config/environment.js
+++ config/environment.js
@@ -1,9 +1,46 @@
 'use strict';
 
+const moduleConfiguration = {
+  types: {
+    application: { definitiveCollection: 'main' },
+    component: { definitiveCollection: 'components' },
+    'component-test': { unresolvable: true },
+    helper: { definitiveCollection: 'components' },
+    'helper-test': { unresolvable: true },
+    renderer: { definitiveCollection: 'main' },
+    template: { definitiveCollection: 'components' },
+    service: { definitiveCollection: 'services' }
+  },
+  collections: {
+    main: {
+      types: ['application', 'renderer']
+    },
+    components: {
+      group: 'ui',
+      types: ['component', 'component-test', 'template', 'helper', 'helper-test'],
+      defaultType: 'component',
+      privateCollections: ['utils']
+    },
+    styles: {
+      group: 'ui',
+      unresolvable: true
+    },
+    utils: {
+      unresolvable: true
+    },
+    services: {
+      types: ['service'],
+      defaultType: 'service',
+      privateCollections: ['utils']
+    }
+  }
+};
+
 module.exports = function(environment) {
   let ENV = {
     environment,
-    modulePrefix: 'simplabs'
+    modulePrefix: 'simplabs',
+    moduleConfiguration
   };
 
   return ENV;
