dab301d4a5f93185c30e107e6e22f5fec5be8bdb
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2019-03-08 11:59:35 +0100

    Update test stubs

--- src/ui/components/service/component-test.ts
+++ src/ui/components/service/component-test.ts
@@ -3,7 +3,7 @@ import { render, setupRenderingTest } from '@glimmer/test-helpers';
 
 const { module, test } = QUnit;
 
-module('Component: service', function(hooks) {
+module('Component: Service', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
@@ -26,7 +26,7 @@ module('Component: service', function(hooks) {
      * assert.dom('p').text('bar');
      * ```
      */
-    await render(hbs`<service />`);
+    await render(hbs`<Service />`);
     assert.ok(this.containerElement.querySelector('div'));
   });
 });
--- src/ui/components/services/component-test.ts
+++ src/ui/components/services/component-test.ts
@@ -3,7 +3,7 @@ import { render, setupRenderingTest } from '@glimmer/test-helpers';
 
 const { module, test } = QUnit;
 
-module('Component: services', function(hooks) {
+module('Component: Services', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
@@ -26,7 +26,7 @@ module('Component: services', function(hooks) {
      * assert.dom('p').text('bar');
      * ```
      */
-    await render(hbs`<services />`);
+    await render(hbs`<Services />`);
     assert.ok(this.containerElement.querySelector('div'));
   });
 });
