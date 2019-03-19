b1bfb26bea05fd1965da13a0ad2849fdf1de7da8
Ghislaine Guerin <ghislaine.guerin@risevision.com>
2019-03-19 07:15:02 +0100

    add stylesheets and route config

--- config/routes-map.js
+++ config/routes-map.js
@@ -7,6 +7,12 @@ module.exports = function() {
     '/service': { component: 'Service' },
     '/case-study': { component: 'CaseStudy' },
     '/expertise': { component: 'Expertise' },
+    '/about': { component: 'About' },
+    '/work': { component: 'Work' },
+    '/talks': { component: 'Talks' },
+    '/calendar': { component: 'Calendar' },
+    '/playbook': { component: 'Playbook' }
+    '/contact': { component: 'Contact' }
   };
 
   return routes;
