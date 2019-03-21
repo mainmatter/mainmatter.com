85d37eb6ec1e4076e176437f2ddd710d38d376f6
Ghislaine Guerin <ghislaineguerin@users.noreply.github.com>
2019-03-21 10:49:52 +0100

    Add page components (#301)
    * add page components
    
    * add stylesheets and route config
    
    * run yarn format
    
    * fix route string
    
    * add about page
    
    * add content
    
    * add grid component
    
    * add page headers
    
    * format changes
    
    * add page components
    
    * add stylesheets and route config
    
    * run yarn format
    
    * fix route string
    
    * add about page
    
    * add content
    
    * add grid component
    
    * add page headers
    
    * format changes
    
    * fix missing class
    
    * add calendar page
    
    * add page components
    
    * add stylesheets and route config
    
    * run yarn format
    
    * fix route string
    
    * add about page
    
    * add content
    
    * add grid component
    
    * add page headers
    
    * format changes
    
    * add page components
    
    * fix missing class
    
    * add calendar page
    
    * fix format
    
    * add comma
    
    * remove template code
    
    * add shape component and update classes
    
    * fix text color
    
    * responsive content and header
    
    * responsive fix
    
    * remove color
    
    * navigation responsive fix
    
    * fix footer margin
    
    * fix classes

--- config/routes-map.js
+++ config/routes-map.js
@@ -6,7 +6,13 @@ module.exports = function() {
     '/services': { component: 'Services' },
     '/service': { component: 'Service' },
     '/cases/trainline': { component: 'TrainlineCaseStudy' },
+    '/about': { component: 'About' },
     '/expertise/ember': { component: 'EmberExpertise' },
+    '/work': { component: 'Work' },
+    '/talks': { component: 'Talks' },
+    '/calendar': { component: 'Calendar' },
+    '/playbook': { component: 'Playbook' },
+    '/contact': { component: 'Contact' },
   };
 
   return routes;
