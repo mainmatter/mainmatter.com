7523f3aeaca764c087c1778efea6ab12a632aba1
wadie <wadie.farhat@simplabs.com>
2019-04-02 20:08:42 +0200

    initial commit

--- config/routes-map.js
+++ config/routes-map.js
@@ -4,7 +4,7 @@ module.exports = function() {
   let routes = {
     '/': { component: 'Homepage' },
     '/services': { component: 'Services' },
-    '/service': { component: 'Service' },
+    '/services/software-engineering': { component: 'SoftwareEngineering' },
     '/cases/trainline': { component: 'TrainlineCaseStudy' },
     '/cases/expedition': { component: 'ExpeditionCaseStudy' },
     '/about': { component: 'About' },
