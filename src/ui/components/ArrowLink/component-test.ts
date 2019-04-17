8a5a23f47d3065ebce6853d3704be0c29b2d362d
Florian Pichler <pichfl@users.noreply.github.com>
2019-04-17 11:17:37 +0200

    ArrowLink component (#336)
    Replace links with arrow with component
    
    - abstracts handling of internal links
    - abstracts link styling
    - fix css-blocks states for SSR build

---
+++ src/ui/components/ArrowLink/component-test.ts
@@ -0,0 +1,32 @@
+import hbs from '@glimmer/inline-precompile';
+import { render, setupRenderingTest } from '@glimmer/test-helpers';
+
+const { module, test } = QUnit;
+
+module('Component: ArrowLink', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    /*
+     * You may pass data into the component through arguments set on the
+     * `testContext`
+     *
+     * For example:
+     *
+     * ```
+     * this.foo = { foo: '123' };
+     *
+     * await render(hbs`<Link @foo={{this.foo}} />`)
+     *
+     * // or
+     *
+     * this.foo = 'bar';
+     * await render(hbs`<p>{{this.foo}}</p>`);
+     *
+     * assert.dom('p').text('bar');
+     * ```
+     */
+    await render(hbs`<ArrowLink />`);
+    assert.ok(this.containerElement.querySelector('div'));
+  });
+});
--- src/ui/components/ShapeCase/component.ts
+++ src/ui/components/ShapeCase/component.ts
@@ -1,19 +1,19 @@
 import Component from '@glimmer/component';
 
 export default class ShapeCase extends Component {
-	public clipId = Math.random().toString(36).substr(2, 5);
+  public clipId = Math.random().toString(36).substr(2, 5);
 
-	get imageClipStyle() {
-		const shouldClip = this.args.clipImage || true;
+  get imageClipStyle() {
+    const shouldClip = this.args.clipImage || true;
 
-		if (!shouldClip) {
-			return '';
-		}
+    if (!shouldClip) {
+      return '';
+    }
 
-		return `clip-path: url(#${this.clipId});`;
-	}
+    return `clip-path: url(#${this.clipId});`;
+  }
 
-	get shapeStyle() {
-		return `fill: ${this.args.color || '#007DF6'}`
-	}
+  get shapeStyle() {
+    return `fill: ${this.args.color || '#007DF6'}`
+  }
 }
