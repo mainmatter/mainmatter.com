456babe9e756d7c720d40e28652cacc562981e04
pichfl <florian.pichler@simplabs.com>
2019-04-25 12:16:05 +0200

    Prefix content components with their generic name
    Making them easier to discern in the list of components.

--- src/ui/components/ShapeFeature/component.ts
+++ src/ui/components/ShapeFeature/component.ts
@@ -1,3 +1,11 @@
 import ShapeBase from '../ShapeBase/component';
 
-export default class ShapeFeature extends ShapeBase {}
+export default class ShapeFeature extends ShapeBase {
+  public maskId = Math.random()
+    .toString(36)
+    .substr(2);
+
+  get backgroundStyle() {
+    return this.args.background && `background-image: url(${this.args.background})`;
+  }
+}
