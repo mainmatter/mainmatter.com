1a4a31194373fc809d0bce188e5d7d84f54bef57
pichfl <florian.pichler@simplabs.com>
2019-05-13 13:43:36 +0200

    Form states and validation

--- src/ui/components/FormContact/component.ts
+++ src/ui/components/FormContact/component.ts
@@ -1,29 +1,107 @@
 import Component, { tracked } from '@glimmer/component';
 
+enum FormState {
+  Error = 'error',
+  Submitting = 'submitting',
+  Success = 'success',
+  Waiting = '',
+}
+
+const minLength = (length, error?) => ({
+  error: () => error || `Please enter at least ${length} characters.`,
+  fn: str => str.length >= length,
+});
+
+const emailIsh = (error?) => ({
+  error: () => error || 'Please enter a valid email address.',
+  fn: str => str.match(/.*@.*\..*/),
+});
+
+const required = (error?) => ({
+  error: () => error || 'Please enter a message.',
+  fn: str => (str || '').trim() !== '',
+});
+
 export default class FormContact extends Component {
   @tracked
-  private isSubmitting: boolean = false;
+  private formState = FormState.Waiting;
 
-  private get formState() {
-    if (this.isSubmitting) {
-      return 'submitting';
-    }
+  @tracked
+  private errors = {};
+
+  @tracked
+  private get totalErrors() {
+    return Object.keys(this.errors).reduce((acc, key) => acc + this.errors[key].length, 0);
+  }
+
+  private validators = {
+    email: [emailIsh()],
+    message: [required()],
+    name: [minLength(2)],
+  };
+
+  @tracked
+  private get isSubmitting(): boolean {
+    return this.formState === FormState.Submitting;
+  }
+
+  @tracked
+  private get isErrored(): boolean {
+    return this.formState === FormState.Error;
+  }
+
+  @tracked
+  private get isSuccess(): boolean {
+    return this.formState === FormState.Success;
   }
 
   public async submit(e) {
     e.preventDefault();
-    this.isSubmitting = true;
 
     let form = e.target;
-    let name = form.querySelector('#name').value;
-    let email = form.querySelector('#email').value;
-    let message = form.querySelector('#message').value;
+    let name = form.querySelector('#name');
+    let email = form.querySelector('#email');
+    let message = form.querySelector('#message');
 
-    await this.sendMessage(name, email, message);
-    this.isSubmitting = false;
+    if (this.formState === FormState.Success) {
+      form.reset();
+
+      this.formState = FormState.Waiting;
+      this.errors = {};
+
+      return;
+    }
+
+    this.formState = FormState.Submitting;
+    this.errors = {};
+
+    this.errors = this.validateFields([name, email, message]);
+
+    if (this.totalErrors > 0) {
+      this.formState = FormState.Waiting;
+
+      return;
+    }
+
+    try {
+      await this.sendMessage(name.value, email.value, message.value);
+    } catch (err) {
+      this.formState = FormState.Error;
+    }
+
+    this.formState = FormState.Success;
   }
 
-  public async sendMessage(name, email, message) {
+  public validate(event) {
+    const errors = this.validateField(event.target);
+
+    this.errors = {
+      ...this.errors,
+      [event.target.id]: errors.length > 0 ? errors : undefined,
+    };
+  }
+
+  private async sendMessage(name, email, message) {
     let response = await fetch('https://guqdu9qkgf.execute-api.eu-central-1.amazonaws.com/production', {
       body: JSON.stringify({ name, email, message }),
       cache: 'no-cache',
@@ -34,5 +112,21 @@ export default class FormContact extends Component {
       mode: 'cors',
       redirect: 'follow',
     });
+  }
+
+  private validateField(field) {
+    return this.validators[field.id].reduce((acc, validator) => {
+      const isValid = validator.fn(field.value);
+
+      if (!isValid) {
+        acc.push(validator.error());
+      }
+
+      return acc;
+    }, []);
+  }
+
+  private validateFields(fields) {
+    return fields.reduce((errors, field) => ({ ...errors, [field.id]: this.validateField(field) }), {});
   }
 }
