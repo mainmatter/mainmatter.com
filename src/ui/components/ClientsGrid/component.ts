62f24e595453dd0a23eaf05ed6fee947151a6450
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-05-20 16:13:27 +0200

    Content improvements (#464)
    * fix client-grid ids
    
    * add embedd to client grid
    
    * add expedition to client grid
    
    * reorder client grid slightly
    
    * add image to timify story
    
    * better text for ember page
    
    * formatting
    
    * fix double space

--- src/ui/components/ClientsGrid/component.ts
+++ src/ui/components/ClientsGrid/component.ts
@@ -20,13 +20,13 @@ const allClients = [
       'Generali approached simplabs when they were looking for support with an internal Ember.js project. We guided their team during the course of the project by teaching and establishing best practices until the project’s successful completion.',
   },
   {
-    id: 'drive',
+    id: 'hopskipdrive',
     src: '/assets/images/logos/hop-skip-drive.svg',
     text:
       'HopSkipDrive builds a safe and dependable transportation solution for schools and families. simplabs built their internal dashboard, allowing them to track rides in real time and react to exceptions immediately.',
   },
   {
-    id: 'hood',
+    id: 'robinhood',
     src: '/assets/images/logos/robin-hood.svg',
     text:
       'RobinHood is a charitable foundation fighting poverty in New York City. simplabs helped them establish an effective engineering process as well as build and release a web based tool for managing Civic User Testing Groups.',
@@ -56,13 +56,43 @@ const allClients = [
       'mvb provides marketing solutions for the german book industry. They approached simplabs when they were looking for external expertise. simplabs performed a workshop, leveling up the team’s expertise and guided the project until its successful completion.',
   },
   {
+    id: 'dim3',
+    src: '/assets/images/logos/dim3.svg',
+    text:
+      'Dim3 delivers software allowing hospitals to manage and optimize their patients’ nutrition plans. They approached simplabs when they faced severe performance problem in their app. We identified and fixed these issues and also found and solved some potential future problems along the way.',
+  },
+  {
+    id: 'timify',
+    src: '/assets/images/logos/timify.svg',
+    text:
+      'Timify is an online appointment scheduling service that connects service providers with clients. When they decided it was time to re-engineer their existing product, they trusted us to set them up for future success.',
+  },
+  {
+    id: 'embedd',
+    src: '/assets/images/logos/embedd.svg',
+    text:
+      'embeDD is the company behind the popular router firmware DD-WRT. We guided embeDD in laying the foundation for the next version of the firmware\'s UI component so they could rely on it for years to come.',
+  },
+  {
+    id: 'Expedition',
+    src: '/assets/images/logos/expedition.svg',
+    text:
+      'Expedition is an online travelling magazin for global citizens. They turned to simplabs when they were looking for guidance to get the most out of their technology stack based on Elixir, Phoenix and Ember.js.',
+  },
+  {
+    id: 'trainline',
+    src: '/assets/images/logos/trainline.svg',
+    text:
+      'Trainline is Europe’s leading independent rail and coach platform.We worked with them to deliver a high - performance mobile web app, along with an improved engineering process, enabling Trainline to achieve their business goals for years to come.',
+  },
+  {
     id: 'ringler',
     src: '/assets/images/logos/ringler.svg',
     text:
       'simplabs helped Ringler meet the deadline for one their projects without sacrificing on quality. Our technology experts joined Ringler’s internal team, increasing the available workforce and sharing their expertise.',
   },
   {
-    id: 'software',
+    id: 'neighbourhoodie',
     src: '/assets/images/logos/neighbourhoodie-software.svg',
     text:
       'simplabs helped Neighbourhoodie successfully complete their first Ember.js project. We conducted an on-site workshop for their team, teaching Ember.js’ fundamental concepts as well as advanced patterns, enabling their team to complete their project successfully.',
@@ -72,25 +102,6 @@ const allClients = [
     src: '/assets/images/logos/loconet.svg',
     text:
       'LoCoNET builds online games for the web. They reached out to simplabs to improve their team’s productivity. We identified and fixed some blocking issues, laid the architectural foundation for some advanced features and conducted workshops to level up their team’s expertise.',
-  },
-  {
-    id: 'dim3',
-    src: '/assets/images/logos/dim3.svg',
-    text:
-      'Dim3 delivers software allowing hospitals to manage and optimize their patients’ nutrition plans. They approached simplabs when they faced severe performance problem in their app. We identified and fixed these issues and also found and solved some potential future problems along the way.',
-  },
-
-  {
-    id: 'trainline',
-    src: '/assets/images/logos/trainline.svg',
-    text:
-      'Trainline is Europe’s leading independent rail and coach platform.We worked with them to deliver a high - performance mobile web app, along with an improved engineering process, enabling Trainline to achieve their business goals for years to come.',
-  },
-  {
-    id: 'timify',
-    src: '/assets/images/logos/timify.svg',
-    text:
-      'Timify is an online appointment scheduling service that connects service providers with clients. When they decided it was time to re-engineer their existing product, they trusted us to set them up for future success.',
   },
 ];
 
