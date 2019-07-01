21c87935bd6112c25fe5a4580dfac1714ac2423a
pichfl <florian.pichler@simplabs.com>
2019-07-01 18:23:37 +0200

    ShapeAcross responsive update

--- src/ui/components/ShapeAcross/component.ts
+++ src/ui/components/ShapeAcross/component.ts
@@ -1,7 +1,28 @@
 import ShapeBase from '../ShapeBase/component';
 
 export default class ShapeAcross extends ShapeBase {
+  maskId = null;
+  flatShapeId = null;
+
+  constructor(options) {
+    super(options);
+
+    this.maskId = Math.random()
+      .toString(36)
+      .substr(2);
+
+    this.flatShapeId = Math.random()
+      .toString(36)
+      .substr(2);
+  }
+
   get backgroundStyle() {
-    return this.args.background && `--shape-color: ${this.args.background}`;
+    let style = '';
+
+    if (this.args.background) {
+      style += `--shape-color: ${this.args.background}`;
+    }
+
+    return style;
   }
 }
