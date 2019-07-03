a41770243444482c1599c9acaa7449eca180aed4
pichfl <florian.pichler@simplabs.com>
2019-07-03 11:48:44 +0200

    Run formatter

--- src/ui/components/ShapeAcross/component.ts
+++ src/ui/components/ShapeAcross/component.ts
@@ -1,17 +1,12 @@
 import ShapeBase from '../ShapeBase/component';
 
 export default class ShapeAcross extends ShapeBase {
-  maskId = null;
-  flatShapeId = null;
+  public maskId = null;
 
   constructor(options) {
     super(options);
 
     this.maskId = Math.random()
-      .toString(36)
-      .substr(2);
-
-    this.flatShapeId = Math.random()
       .toString(36)
       .substr(2);
   }
