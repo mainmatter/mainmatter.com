719eb38826ece93ee927167735500c791daa1534
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-07-31 20:39:38 +0200

    add og:type, twitter:card meta tags (#671)
    * add twitter:card meta tag
    
    * add og:type meta tag
    
    * define private pageType field on Header component

--- src/ui/components/Header/component.ts
+++ src/ui/components/Header/component.ts
@@ -2,13 +2,15 @@ import Component from '@glimmer/component';
 
 export default class Header extends Component {
   public documentTitle = '';
+  private pageType = 'website';
 
   constructor(options) {
     super(options);
 
     let documentTitle = this.args.documentTitle === undefined ? this.args.title : this.args.documentTitle;
-    
     this.documentTitle = formatformatDocumentTitle(documentTitle);
+
+    this.pageType = this.args.pageType || this.pageType;
   }
 }
 
