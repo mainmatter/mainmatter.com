66b7a614ead489e0ab6c3f440b82bd1124a05de2
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-07-31 10:36:26 +0200

    rename pageTitle arg to documentTitle

--- src/ui/components/Header/component.ts
+++ src/ui/components/Header/component.ts
@@ -4,11 +4,11 @@ export default class Header extends Component {
   constructor(options) {
     super(options);
 
-    let pageTitle = this.args.pageTitle === undefined ? this.args.title : this.args.pageTitle;
-    this.pageTitle = formatPageTitle(pageTitle);
+    let documentTitle = this.args.documentTitle === undefined ? this.args.title : this.args.documentTitle;
+    this.documentTitle = formatformatDocumentTitle(documentTitle);
   }
 }
 
-function formatPageTitle(title) {
+function formatformatDocumentTitle(title) {
   return `${title ? `${title} | ` : ''}simplabs`;
 }
