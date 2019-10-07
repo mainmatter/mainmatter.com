b412f726ea421126c70e8091239bc19717fe972a
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2019-10-07 14:17:09 +0200

    Improve preloading in service worker (#713)
    * don't preload blog assets in service worker
    * preload blog assets lazily (only on visit to blog)
    * do not call service worker in SSR context

---
+++ src/ui/components/LazyPreloadTrigger/component.ts
@@ -0,0 +1,20 @@
+import Component from '@glimmer/component';
+
+export default class LazyPreloadTrigger extends Component {
+  private appState: IAppState;
+
+  constructor(options) {
+    super(options);
+
+    this.preload();
+  }
+
+  private preload() {
+    if (!this.appState.isSSR) {
+      let { controller: serviceWorkerController } = navigator.serviceWorker;
+      if (serviceWorkerController) {
+        serviceWorkerController.postMessage({ preload: this.args.bundle });
+      }
+    }
+  }
+}
