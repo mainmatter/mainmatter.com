3b3a185f9447d4a46097bdb2b6526161d4717751
Ghislaine Guerin <ghislaine.guerin@simplabs.com>
2020-02-04 16:06:11 +0100

    reduce image size and replace css variables

--- src/ui/components/WorkWithUs/component.ts
+++ src/ui/components/WorkWithUs/component.ts
@@ -1,9 +1,9 @@
 import Component, { tracked } from '@glimmer/component';
 
 const photos = {
-  'chris': '/assets/images/photos/chris@604.png 604w, /assets/images/photos/chris@1208.png 1208w',
-  'ghislaine': '/assets/images/photos/ghislaine@604.png 604w, /assets/images/photos/ghislaine@1208.png 1208w',
-  'marco': '/assets/images/photos/marco@604.png 604w, /assets/images/photos/marco@1208.png 1208w',
+  'chris': '/assets/images/photos/chris@604.png 604w',
+  'ghislaine': '/assets/images/photos/ghislaine@604.png 604w',
+  'marco': '/assets/images/photos/marco@604.png 604w',
 };
 
 export default class WorkWithUs extends Component {
