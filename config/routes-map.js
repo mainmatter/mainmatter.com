cc9d723bb23ce1eaa29c3bf86f0712561a571cbe
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2020-02-05 15:49:37 +0100

    add page on product design

--- config/routes-map.js
+++ config/routes-map.js
@@ -86,6 +86,7 @@ module.exports = function() {
       component: 'PageFullStackEngineering',
     },
     '/services/team-augmentation': { component: 'PageTeamAugmentation' },
+    '/services/product-design': { component: 'PageProductDesign' },
     '/services/tutoring': { component: 'PageTutoring' },
     '/talks': { component: 'PageTalks', bundle: { asset: '/talks.js', module: '__talks__' } },
     '/why-simplabs': { component: 'PageWhySimplabs' },
