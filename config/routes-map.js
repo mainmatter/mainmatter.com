4fa0d8d480874442c18826ccfef21eca30ba610c
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2020-02-17 13:01:59 +0100

    Cleanup shape feature (#893)
    * Update shape component (#777)
    
    * replace shape
    
    * fix polygon angles and remove changes from baseline
    
    * fix format
    
    * fix other shape instances
    
    * update footer component
    
    * add team member photo
    
    * Updates content of Services page (#819)
    
    * add leftAlign state to ArrowLink css block
    
    * update services page content and layout
    
    * use lead typography for sub header
    
    * use title case
    
    * [Services] Update content for training and tutoring page (#844)
    
    * Update content for training and tutoring page
    
    * run format
    
    * Update content for team augmentation page (#843)
    
    * Update content for homepage (#845)
    
    * remove digital products link until the page is created
    
    * complete footer shape
    
    * fix bottom clip
    
    * add team member
    
    * optimize images
    
    * format
    
    * add updated footer
    
    * fix footer and remove colors from component
    
    * run format
    
    * simplify footer component
    
    * run format
    
    * fix component
    
    * make WorkWithUs photo customizable
    
    * format WorkWithUs component
    
    * optimize images and fix photo scale
    
    * fix code formatting
    
    * capitalize team member name in alt attribute
    
    * reduce image size and replace css variables
    
    * render 604px version of the photo directly
    
    * add page on product design
    
    * Add digital product page (#863)
    
    * add digital products page
    
    * link digital products page from services page
    
    * remove ember shape from digital products page
    
    * add digital products link to homepage
    
    * fix header label
    
    * add link to Product Strategy section
    
    * update Product Strategy bullet points
    
    Co-authored-by: Ricardo Mendes <rokusu@gmail.com>
    
    * Add Product Development page (#864)
    
    * add product development page
    
    * add missing link
    
    Co-authored-by: Ricardo Mendes <rokusu@gmail.com>
    
    * update cssblocks syntax
    
    * Fix formatting
    
    * Style lists in services pages (#879)
    
    * Style list in product development page
    
    * move style to list block and tweak spacing
    
    * style list in product design page
    
    * style list on product design page
    
    * style lists in digital products page
    
    * remove hasImage state of ShapeFeature
    
    * don't use figure/figcaption elements without image
    
    Co-authored-by: Ghislaine Guerin <ghislaineguerin@users.noreply.github.com>
    Co-authored-by: Ricardo Mendes <rokusu@gmail.com>

--- config/routes-map.js
+++ config/routes-map.js
@@ -85,7 +85,12 @@ module.exports = function() {
     '/services/full-stack-engineering': {
       component: 'PageFullStackEngineering',
     },
+    '/services/digital-products': {
+      component: 'PageDigitalProducts',
+    },
     '/services/team-augmentation': { component: 'PageTeamAugmentation' },
+    '/services/product-development': { component: 'PageProductDevelopment' },
+    '/services/product-design': { component: 'PageProductDesign' },
     '/services/tutoring': { component: 'PageTutoring' },
     '/talks': { component: 'PageTalks', bundle: { asset: '/talks.js', module: '__talks__' } },
     '/why-simplabs': { component: 'PageWhySimplabs' },
