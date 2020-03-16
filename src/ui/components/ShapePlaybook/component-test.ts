f5ef85b359cf36ad35a7d88d6a21bc100b2576b4
Ricardo Mendes <rokusu@gmail.com>
2020-03-16 23:45:42 +0000

    add playbook landing page layout with placeholder content
    Updated the layout to match the new design proposal.
    Created a new shape for the book download block which
    will need further adjustment once we have the final
    image.
    All images and copy are to be considered placeholders.

---
+++ src/ui/components/ShapePlaybook/component-test.ts
@@ -0,0 +1,33 @@
+import hbs from '@glimmer/inline-precompile';
+import { render } from '@glimmer/test-helpers';
+import { setupRenderingTest } from '../../../utils/test-helpers/setup-rendering-test';
+
+const { module, test } = QUnit;
+
+module('Component: ShapeFeature', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    await render(hbs`<ShapeFeature />`);
+
+    assert.ok(this.containerElement.querySelector('div'));
+  });
+
+  module('when invoked with a @srcset', function() {
+    test('it renders figure and figcaption elements', async function(assert) {
+      await render(hbs`<ShapeFeature @srcset="./some/image/source.png" />`);
+
+      assert.ok(this.containerElement.querySelector('figure'));
+      assert.ok(this.containerElement.querySelector('figcaption'));
+    });
+  });
+
+  module('when invoked without a @srcset', function() {
+    test('it does not render figure and figcaption elements', async function(assert) {
+      await render(hbs`<ShapeFeature />`);
+
+      assert.notOk(this.containerElement.querySelector('figure'));
+      assert.notOk(this.containerElement.querySelector('figcaption'));
+    });
+  });
+});
