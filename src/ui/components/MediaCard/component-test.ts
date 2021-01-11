38687ecd47c3117ad61fae658699bca01e98feab
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2021-01-11 17:19:11 +0100

    make MediaCard image dynamic

--- src/ui/components/MediaCard/component-test.ts
+++ src/ui/components/MediaCard/component-test.ts
@@ -8,10 +8,11 @@ module('Component: MediaCard', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    await render(hbs`<MediaCard @label="Label" @title="Titel">Content</MediaCard>`);
+    await render(hbs`<MediaCard @label="Label" @title="Titel" @image="https://domain.com/source.png">Content</MediaCard>`);
 
     assert.ok(this.containerElement.textContent.includes('Label'));
     assert.ok(this.containerElement.textContent.includes('Titel'));
     assert.ok(this.containerElement.textContent.includes('Content'));
+    assert.equal(this.containerElement.querySelector('img').src, 'https://domain.com/source.png');
   });
 });
