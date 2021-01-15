17d6f4b084f02a4541838e475e28923bcfa27ea8
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2021-01-15 18:05:00 +0100

    fix capitalization of CLARK (#1432)

--- src/ui/components/ClientsGrid/component.ts
+++ src/ui/components/ClientsGrid/component.ts
@@ -38,10 +38,10 @@ const allClients = [
   },
   {
     id: 'clark',
-    name: 'Clark',
+    name: 'CLARK',
     src: '/assets/images/logos/clark.svg',
     text:
-      'simplabs was approached by Clark when they were looking for guidance on how to solidify the codebase of their insurance management app. We conducted a thorough review, presented the workshop to the team and layed out a plan for addressing the identified issues.',
+      'simplabs was approached by CLARK when they were looking for guidance on how to solidify the codebase of their insurance management app. We conducted a thorough review, presented the workshop to the team and layed out a plan for addressing the identified issues.',
   },
   {
     id: 'xbav',
