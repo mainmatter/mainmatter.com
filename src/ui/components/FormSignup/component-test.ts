674b54a0a32b1ee211496f1bdfc4ec5fb5cd2a55
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2021-01-22 13:08:24 +0100

    fix signup form

--- src/ui/components/FormSignup/component-test.ts
+++ src/ui/components/FormSignup/component-test.ts
@@ -7,26 +7,8 @@ module('Component: FormSignup', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    /*
-     * You may pass data into the component through arguments set on the
-     * `testContext`
-     *
-     * For example:
-     *
-     * ```
-     * this.foo = { foo: '123' };
-     *
-     * await render(hbs`<FormSignup @foo={{this.foo}} />`)
-     *
-     * // or
-     *
-     * this.foo = 'bar';
-     * await render(hbs`<p>{{this.foo}}</p>`);
-     *
-     * assert.dom('p').text('bar');
-     * ```
-     */
     await render(hbs`<FormSignup />`);
-    assert.ok(this.containerElement.querySelector('div'));
+
+    assert.ok(this.containerElement.querySelector('form'));
   });
 });
