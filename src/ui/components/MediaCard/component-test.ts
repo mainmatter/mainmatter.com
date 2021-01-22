18579dbb187696258efe7042e6264bf15fcb5f1c
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2021-01-22 16:36:48 +0100

    allow linking MediaCard titles

--- src/ui/components/MediaCard/component-test.ts
+++ src/ui/components/MediaCard/component-test.ts
@@ -21,4 +21,58 @@ module('Component: MediaCard', function(hooks) {
 
     assert.ok(this.containerElement.textContent.includes('04:54'));
   });
+
+  test('it adds a "data-internal" attribute for relative internal links', async function(assert) {
+    await render(hbs`<MediaCard @link="/link" />`);
+
+    assert.ok(this.containerElement.querySelector('a').dataset.internal !== undefined);
+  });
+
+  test('it does not add a "data-internal" attribute for absolute internal links', async function(assert) {
+    await render(hbs`<MediaCard @link="https://simplabs.com/link" />`);
+
+    assert.ok(this.containerElement.querySelector('a').dataset.internal === undefined);
+  });
+
+  test('it does not add a "data-internal" attribute for external links', async function(assert) {
+    await render(hbs`<MediaCard @link="https://github.com" />`);
+
+    assert.ok(this.containerElement.querySelector('a').dataset.internal === undefined);
+  });
+
+  test('it does not add a "target" attribute for relative internal links', async function(assert) {
+    await render(hbs`<MediaCard @link="/link" />`);
+
+    assert.notOk(this.containerElement.querySelector('a').target);
+  });
+
+  test('it does not add a "target" attribute for absolute internal links', async function(assert) {
+    await render(hbs`<MediaCard @link="https://simplabs.com/link" />`);
+
+    assert.notOk(this.containerElement.querySelector('a').target);
+  });
+
+  test('it adds a "target" attribute with value "_blank" for external links', async function(assert) {
+    await render(hbs`<MediaCard @link="https://github.com" />`);
+
+    assert.equal(this.containerElement.querySelector('a').target, '_blank');
+  });
+
+  test('it does not add a "rel=noopener" attribute for relative internal links', async function(assert) {
+    await render(hbs`<MediaCard @link="/link" />`);
+
+    assert.notOk(this.containerElement.querySelector('a').rel);
+  });
+
+  test('it does not add a "rel=noopener" attribute for absolute internal links', async function(assert) {
+    await render(hbs`<MediaCard @link="https://simplabs.com/link" />`);
+
+    assert.notOk(this.containerElement.querySelector('a').rel);
+  });
+
+  test('it adda a "rel=noopener" attribute for external links', async function(assert) {
+    await render(hbs`<MediaCard @link="https://github.com" />`);
+
+    assert.equal(this.containerElement.querySelector('a').rel, 'noopener');
+  });
 });
