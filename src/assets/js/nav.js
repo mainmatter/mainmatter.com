6eb36ec13c9fb95ab0f899dbf7ebbb1144ea29a4
Marianne Ferguson <hello@marianneciara.com>
2022-07-12 17:56:40 +0100

    refactor nav logic for new full screen menu, desktop submenu styling mostly complete

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,82 +1,51 @@
 export class Nav {
   constructor(element) {
     this.container = element;
-    this.dropdownToggles = this.container.querySelectorAll(
-      "[data-dropdown-toggle]"
-    );
-    this.menuToggle = this.container.querySelector("[data-menu-toggle]");
-    this.dropdowns = this.container.querySelectorAll("[data-dropdown]");
+    this.menuToggles = this.container.querySelectorAll("[data-menu-toggle]");
+    this.menuCloseButtons = this.container.querySelectorAll("[data-menu-close");
 
     this.bindEvents();
   }
 
   bindEvents() {
-    this.dropdownToggles.forEach((dropdownToggle) => {
-      dropdownToggle.addEventListener("click", () => {
-        if (dropdownToggle.getAttribute("aria-expanded") === "true") {
-          dropdownToggle.setAttribute("aria-expanded", "false");
+    this.menuToggles.forEach((menuToggle) => {
+      menuToggle.addEventListener("click", () => {
+        if (menuToggle.getAttribute("aria-expanded") === "true") {
+          this.closeMenu(menuToggle);
         } else {
-          dropdownToggle.setAttribute("aria-expanded", "true");
+          this.openMenu(menuToggle);
         }
       });
     });
 
-    this.dropdowns.forEach((dropdown) => {
-      dropdown.addEventListener("focusout", (event) => {
-        if (!event.relatedTarget.closest("[data-dropdown]")) {
-          const toggleName = dropdown.getAttribute("id");
-          const toggle = document.querySelector(
-            `[data-dropdown-toggle="${toggleName}"]`
-          );
-
-          toggle.setAttribute("aria-expanded", "false");
+    this.menuCloseButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const toggle = button.closest("[data-submenu")?.querySelector("[data-menu-toggle]");
+        if (toggle.getAttribute("aria-expanded") === "true") {
+          this.closeMenu(toggle);
+        } else {
+          this.openMenu(toggle);
         }
       });
-    });
-
-    document.addEventListener("click", (event) => {
-      if (!event.target.closest("[data-dropdown-wrapper]")) {
-        this.dropdownToggles.forEach((dropdownToggle) => {
-          this.closeDropdown(dropdownToggle);
-        });
-      }
-    });
-
-    this.menuToggle.addEventListener("click", () => {
-      if (this.menuToggle.getAttribute("aria-expanded") === "true") {
-        this.closeMenu();
-      } else {
-        this.openMenu();
-      }
     });
 
     // Escape key handler
     document.addEventListener("keydown", (event) => {
       if (event.key === "Escape") {
-        this.dropdownToggles.forEach((dropdownToggle) => {
-          this.closeDropdown(dropdownToggle);
+        this.menuToggles.forEach((menuToggle) => {
+          this.closeMenu(menuToggle);
         });
-
-        if (this.menuToggle.getAttribute("aria-expanded") === "true") {
-          this.closeMenu();
-        }
       }
     });
   }
 
-  closeDropdown(toggle) {
-    if (toggle.getAttribute("aria-expanded") === "true") {
-      toggle.setAttribute("aria-expanded", "false");
-      toggle.focus();
-    }
-  }
-
-  closeMenu() {
+  closeMenu(menuToggle) {
     const siblings = this.getAllSiblings(this.container);
-    this.menuToggle.setAttribute("aria-expanded", "false");
+    menuToggle.setAttribute("aria-expanded", "false");
     siblings.forEach((sibling) => sibling.removeAttribute("inert"));
-    this.menuToggle.focus();
+    menuToggle.focus();
     document.documentElement.style.overflow = "";
+    document.body.classList.remove("menu-open");
   }
 
   getAllSiblings(element) {
@@ -84,10 +53,11 @@ export class Nav {
     return children.filter((child) => child !== element);
   }
 
-  openMenu() {
+  openMenu(menuToggle) {
     const siblings = this.getAllSiblings(this.container);
-    this.menuToggle.setAttribute("aria-expanded", "true");
+    menuToggle.setAttribute("aria-expanded", "true");
     siblings.forEach((sibling) => sibling.setAttribute("inert", true));
     document.documentElement.style.overflow = "hidden";
+    document.body.classList.add("menu-open");
   }
 }
