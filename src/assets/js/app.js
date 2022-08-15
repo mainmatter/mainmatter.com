c806f876d3213b77805c6b64608cfd92c1041310
Marianne Ferguson <hello@marianneciara.com>
2022-08-15 15:40:01 +0100

    add pinned on scroll animation functionality

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,6 +4,7 @@ import bindAccordions from "./accordions";
 import { Nav } from "./nav";
 import { ContactForm } from "./contact-form";
 import { LogoList } from "./logo-list";
+import { ScrollSlides } from "./scroll-slides";
 
 const navElement = document.getElementById("nav");
 new Nav(navElement);
@@ -13,6 +14,9 @@ if (contactForm) new ContactForm(contactForm);
 
 const logoList = document.getElementById("logo-list");
 if (logoList) new LogoList(logoList);
+
+const scrollSlides = document.getElementById("scroll-slides");
+if (scrollSlides) new ScrollSlides(scrollSlides);
 
 new Animations();
 bindAccordions();
