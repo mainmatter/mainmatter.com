4ac867ef8d055daf156c0798c5a840aec6a6808c
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2022-09-15 15:30:49 +0200

    remove debug log

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import commonjs from "rollup-plugin-commonjs";
 import nodeResolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
-console.log(nodeResolve);
+
 export default {
   input: "src/assets/js/app.js",
   output: {
