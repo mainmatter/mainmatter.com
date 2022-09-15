8a1b894d39c7ec4b10c909af1f32173f4f270e27
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2022-09-15 21:07:01 +0200

    add lighthouse

---
+++ .lighthouserc.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  ci: {
+    collect: {
+      settings: {
+        skipAudits: ['is-on-https', 'redirects-http', 'uses-http2', ''],
+      },
+    },
+    assert: {
+      assertions: {
+        'is-on-https': 'off',
+        'redirects-http': 'off',
+        'uses-http2': 'off',
+      },
+    },
+  },
+};
