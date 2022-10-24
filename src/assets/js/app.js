e69318925d0954ca239bdc149c7cfb5eac9de4d5
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2022-10-24 16:36:02 +0200

    init Sentry

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,9 +1,16 @@
+import * as Sentry from "@sentry/browser";
 import "wicg-inert";
 import { Animations } from "./animations";
 import { Nav } from "./nav";
 import { ContactForm } from "./contact-form";
 import { LogoList } from "./logo-list";
 import bindSelectDropdowns from "./select";
+
+if (window.location.host === "mainmatter.com") {
+  Sentry.init({
+    dsn: "https://43f7627909d94dc4a769340ad730f1a2@o68744.ingest.sentry.io/4504039028817920"
+  });
+}
 
 const navElement = document.getElementById("nav");
 new Nav(navElement);
