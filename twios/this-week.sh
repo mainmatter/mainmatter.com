65889cde0f27be18610edd1a60b744438e51bd21
BobrImperator <bartholomew.dudzik@gmail.com>
2023-09-21 18:42:27 +0200

    feat: add different build twios

--- twios/this-week.sh
+++ twios/this-week.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-chmod +x twios/this-week-in-open-source
-
-$(pwd)/twios/this-week-in-open-source --config-path=$(pwd)/config/this-week-in-open-source.config.json
+twios/this-week-in-open-source --config-path=config/this-week-in-open-source.config.json
