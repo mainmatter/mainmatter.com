0746f9fade4ff5406674ecdf9bfe30eafda64803
BobrImperator <bartholomew.dudzik@gmail.com>
2023-09-21 16:02:38 +0200

    chore(ci): test generate against this pr branch

--- bin/this-week.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./bin/this-week-in-open-source --config-path=config/this-week-in-open-source.config.json
---
+++ twios/this-week.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+twios/this-week-in-open-source --config-path=config/this-week-in-open-source.config.json
