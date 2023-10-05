d59c0468cf5527b5018a5a63799b91e54b3c6967
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2023-10-05 17:13:20 +0200

    use unique titles for twios pages

--- src/twios/twios.11tydata.js
+++ src/twios/twios.11tydata.js
@@ -1,6 +1,11 @@
 module.exports = {
   layout: "twios-post",
   eleventyComputed: {
+    title: function (data) {
+      const post = data.collections.twios.find(post => post.fileSlug === data.page.fileSlug);
+      const number = post?.data?.number;
+      return `This Week in Open Source at Mainmatter${number ? ` – #${number}` : ""}`;
+    },
     permalink: function (data) {
       return (
         "/this-week-in-open-source/" +
