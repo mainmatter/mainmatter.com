1fb10f0e22f40ef107b7be63b5f7deaced617901
Nora Burkhardt <39055470+yaigrie@users.noreply.github.com>
2023-12-04 17:32:59 +0100

    Revert "Revert "hide ctabtn at bottom of page""
    This reverts commit d9589dbab92c28d4a3caea71f1e79f6f55125a77.

--- src/assets/js/scroll.js
+++ src/assets/js/scroll.js
@@ -3,11 +3,39 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
  
 gsap.registerPlugin( ScrollTrigger );
 
-gsap.from(".floating-btn", {
+/* gsap.from(".floating-btn", {
   y: 400,
   scrollTrigger: {
     trigger: ".trigger",
     start: "top 50%",
-    toggleActions: "play pause reverse reset",
+    toggleActions: "play complete reverse restart",
   }
   });
+
+  gsap.to(".floating-btn", {
+    opacity: 0,
+    scrollTrigger: {
+      trigger: ".end-trigger",
+      start: "bottom 90%",
+      toggleActions: "play none reset none",
+    }
+    }); */
+
+
+    const anim = gsap.from('.floating-btn', {
+        y: 400,
+        paused: true
+      });
+      
+      const playST = ScrollTrigger.create({
+        trigger:'.trigger', 
+        start:'top 50%', 
+        onEnter: () => anim.play(),
+        onLeaveBack: () => anim.reverse(),
+      });
+      
+      const resetST = ScrollTrigger.create({
+        trigger:'.end-trigger',
+        onEnter: () => anim.reverse(),
+        onLeaveBack: () => anim.play(),
+      });
