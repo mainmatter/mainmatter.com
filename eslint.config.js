da9e75b23b6705e6991c65cf39bdb43f9ced774d
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2024-06-05 11:30:00 +0200

    migrate ESLint config to new format

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+const prettier = require("eslint-plugin-prettier");
+const eslintConfigPrettier = require("eslint-config-prettier");
+const node = require("eslint-plugin-node");
+
+module.exports = [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: { prettier, node },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+  {
+    // Test files:
+    files: ["src/assets/**/*.js"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+];
