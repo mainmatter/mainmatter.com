042cd595227a5f1a3ab91bb7bc2306479751402d
umbrellarocket <80921428+umbrellarocket@users.noreply.github.com>
2026-03-16 11:07:30 +0100

    add(cookie banner): styling and GTM

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,7 +4,7 @@ import { Nav } from "./nav";
 import { ContactForm } from "./contact-form";
 import { LogoList } from "./logo-list";
 import bindSelectDropdowns from "./select";
-import './silktide-consent-manager.js';
+import "./utils/silktide-consent-manager.js";
 
 if (window.location.host === "mainmatter.com" || window.location.host.includes("deploy-preview")) {
   Sentry.init({
@@ -33,43 +33,55 @@ bindSelectDropdowns();
 
 //Silktide consent manager
 window.silktideConsentManager.init({
-  text: {
   prompt: {
-    description: '<p>We use cookies to enhance your experience.</p>',
-    acceptAllButtonText: 'Accept all',
-    acceptAllButtonAccessibleLabel: 'Accept all cookies',
-    rejectNonEssentialButtonText: 'Reject non-essential',
-    rejectNonEssentialButtonAccessibleLabel: 'Reject all non-essential cookies',
-    preferencesButtonText: 'Preferences',
-    preferencesButtonAccessibleLabel: 'Manage cookie preferences',
+    position: "bottomLeft",
   },
-  preferences: {
-    title: 'Customize your preferences',
-    description: '<p>Choose which cookies you want to accept.</p>',
-    saveButtonText: 'Save and close',
-    saveButtonAccessibleLabel: 'Save your cookie preferences',
-    creditLinkText: ' ',
-    creditLinkAccessibleLabel: ' ',
-  }
-},
+  text: {
+    prompt: {
+      description: "<p>We use cookies to enhance your experience.</p>",
+      acceptAllButtonText: "Accept all",
+      acceptAllButtonAccessibleLabel: "Accept all cookies",
+      rejectNonEssentialButtonText: "Reject non-essential",
+      rejectNonEssentialButtonAccessibleLabel: "Reject all non-essential cookies",
+      preferencesButtonText: "Preferences",
+      preferencesButtonAccessibleLabel: "Manage cookie preferences",
+    },
+    preferences: {
+      title: "Customize your preferences",
+      description: "<p>Choose which cookies you want to accept.</p>",
+      saveButtonText: "Save and close",
+      saveButtonAccessibleLabel: "Save your cookie preferences",
+      creditLinkText: " ",
+      creditLinkAccessibleLabel: " ",
+    },
+  },
   consentTypes: [
     {
-      id: 'essential',
-      label: 'Essential',
-      description: 'Required for the website to function. Cannot be switched off.',
-      required: true
-    },
-    {
-      id: 'analytics',
-      label: 'Analytics',
-      description: 'Help us understand how visitors use the site.',
+      id: "analytics",
+      label: "Analytics",
+      description: "Help us understand how visitors use the site.",
       defaultValue: true,
     },
     {
-      id: 'marketing',
-      label: 'Marketing',
-      description: 'Used to deliver personalised ads.',
-      defaultValue: false,
-    }
-  ]
+      id: "marketing",
+      label: "Marketing",
+      description: "Used to deliver personalised ads.",
+      defaultValue: true,
+      gtag: ["ad_storage", "ad_user_data", "ad_personalization"],
+      scripts: [
+        {
+          url: "https://www.googletagmanager.com/gtag/js?id=AW-962403954",
+          load: "async",
+        },
+      ],
+      onAccept: function () {
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+        gtag("config", "AW-962403954");
+      },
+    },
+  ],
 });
