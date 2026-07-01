5f47ceedcf3400bbfa7187cfc0d1b898545d9882
Jonas Kruckenberg <iterpre@protonmail.com>
2026-07-01 13:04:25 +0200

    update book

--- mdbook/c-to-rust-migration-book/searcher-c2a407aa.js
+++ mdbook/c-to-rust-migration-book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-fe43f3a9.js',
+                path_to_root + 'searchindex-321107b2.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
